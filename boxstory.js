e14d9212fcd02e0bf9b20474d2d3204be5ed6715
caglarorhan <caglaror@gmail.com>
2020-11-30 23:29:31 -0600

    Bottom tip banner beta

--- boxstory.js
+++ boxstory.js
@@ -30,31 +30,34 @@ const boxStory = {
             })
         }
         //box creating...
-            let widthOfBox = boxData.width.includes('%') ? boxData.width : boxData.width;
-            let leftOfBox = boxData.left;
+            let widthOfBox = boxData.width.includes('%') ? boxStory.vw()*(parseInt(boxData.width)/100) : boxData.width;
+            let leftOfBox =  boxData.left.includes('auto') ? (boxStory.vw()-widthOfBox)/2  :  boxData.left;
             let topOfBox = boxData.top;
+            let tip_size=0;
+
+
             switch (boxData.side) {
                 case "right":
-                    leftOfBox = boxStory.vw() - parseInt(boxData.width);
+                    leftOfBox = boxStory.vw() - parseInt(widthOfBox);
+                    if(boxData.switch_id!==null || boxData.switch_id!==''){
+                        leftOfBox = boxStory.vw();
+                    }
                     break;
                 case "left":
                     leftOfBox = 0;
                     break;
                 case "bottom":
-                    console.log('Bottom geldi')
                     topOfBox = boxStory.vh() - parseInt(boxData.height);
-                    leftOfBox = (boxStory.vw() - parseInt(boxData.width)) / 2;
+                    if(boxData.switch_id!==null || boxData.switch_id!==''){
+                        topOfBox = boxStory.vh();
+                    }
                     break;
             }
 
-            box.style.cssText = `width:${widthOfBox}; height: ${boxData.height}; left:${leftOfBox}px; top:${topOfBox}px; position:${boxData.position}; margin:${boxData.margin}; background-color:${boxData.bgColor}; z-index: ${boxData.z_index} border-radius:${boxData.border_radius}; border: ${boxData.border}; box-shadow:${boxData.box_shadow} ; box-sizing: border-box;`;
+            box.style.cssText = `width:${widthOfBox}px; height: ${boxData.height}; left:${leftOfBox}px; top:${topOfBox}px; position:${boxData.position}; margin:${boxData.margin}; background-color:${boxData.bgColor}; z-index: ${boxData.z_index} border-radius:${boxData.border_radius}; border: ${boxData.border}; box-shadow:${boxData.box_shadow} ; box-sizing: border-box;`;
             box.id = 'box_' + boxData.content_id;
             box.innerHTML = boxData.content;
             box.append(boxCloseButton);
-
-            box.addEventListener('click',event=>{
-                event.stopPropagation();
-            })
 
             if (document.querySelector('#backdrop_' + boxData.content_id)) {
                 document.querySelector('#backdrop_' + boxData.content_id).append(box);
@@ -64,20 +67,22 @@ const boxStory = {
                 document.querySelector('#box_' + boxData.content_id).style.display='none';
             }
     },
-    createAnimation(boxData, animationName) {
+    createAnimation(boxData, animationData) {
         if(!document.querySelector('#box_' + boxData.content_id)){
             this.createBox(boxData);
         }
 
-        const targetCSS = document.createElement('style');
-        let animationSpecsText = "";
-        console.log(animationName)
-        Object.entries(boxData.animations[animationName].animation_specs).forEach(([k, v]) => {
-            if (v) {
-                animationSpecsText += `${k}:${v}; `;
-            }
-        })
-        targetCSS.innerHTML = `
+
+        animationData.animation_name.forEach(animationName =>{
+            let targetCSS = document.createElement('style');
+            let animationSpecsText = "";
+            Object.entries(boxData.animations[animationName].animation_specs).forEach(([k, v]) => {
+                if (v) {
+                    animationSpecsText += `${k}:${v}; `;
+                }
+            })
+
+            targetCSS.innerHTML = `
         .${animationName}_${boxData.content_id}{
             animation-name: ${animationName};
             ${animationSpecsText}
@@ -87,10 +92,12 @@ const boxStory = {
                                 }
                                
             `;
-        // CSS rule control
-        if(!Object.values(document.styleSheets).map(rlz=>rlz.rules[0].selectorText).includes("."+animationName + "_" + boxData.content_id)){
-            document.head.append(targetCSS);
-        }
+            // CSS rule control
+            if(!Object.values(document.styleSheets).map(rlz=>rlz.rules[0].selectorText).includes("."+animationName + "_" + boxData.content_id)){
+                document.head.append(targetCSS);
+            }
+        })
+
 
     },
     init(json_src) {
@@ -112,64 +119,82 @@ const boxStory = {
 
         })
     },
-    action(boxData){
-        console.log('action giris')
-        Object.keys(boxData.scenarios).forEach(scenarioName=>{
-            let scenario = boxData.scenarios[scenarioName];
-            const triggerElement = (scenario.event_source!=="window")?document.querySelector(scenario.event_source +'_' + boxData.content_id):document;
+    // animation applicator
+    animApplicator(boxData, scenarioName, animationData){
+    let switchKey = document.querySelector(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id).checked;
 
-                    scenario.animations.forEach((animationData, animIndex) => {
-                        this.createAnimation(boxData, animationData.animation_name); //CSS animasyonlari olusturulup style icine gomuldu
-                        console.log('--->'+scenario.event_source +':'+ scenario.event);
-                        if(scenario.event_source==="window" && scenario.event==='load'){ // window.load ise dogrudan class ekle (CSS animasyon olusmustu zaten)
-                            console.log('evet')
-                            document.querySelector("#box_" + boxData.content_id).style.display='';
-                            document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name + "_" + boxData.content_id);
-                        }
-                        // animation applicator
-                        function animApplicator(){
-                            document.querySelector("#box_" + boxData.content_id).classList.remove(animationData.animation_name + "_" + boxData.content_id);
-                            if (animationData.repetitive === "true" || animationData.repetitive) {
-                                window.requestAnimationFrame(function (time) {
-                                    window.requestAnimationFrame(function (time) {
-                                        if(boxData.backdrop || boxData.backdrop ==='true'){
-                                            document.querySelector('#backdrop_' + boxData.content_id).style.display='block';
-                                        }
-                                        document.querySelector('#box_' + boxData.content_id).style.display='block';
-                                        document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name + "_" + boxData.content_id);
-                                    });
-                                });
-                            } else {
-                                document.querySelector('#box_' + boxData.content_id).style.display='block';
-                                document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name + "_" + boxData.content_id);
-                            }
-                        }
-                        //repetative degerine gore tek sefer implemente edilecek veya requestAnimationFrame kullanilacak
-                        triggerElement.addEventListener(scenario.event,animApplicator)
+    document.querySelector("#box_" + boxData.content_id).classList.remove(animationData.animation_name[this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id)] + "_" + boxData.content_id);
+    if (animationData.repetitive === "true" || animationData.repetitive===true) {
+        window.requestAnimationFrame(function (time) {
+            window.requestAnimationFrame(function (time) {
+                if(boxData.backdrop === true || boxData.backdrop ==='true'){
+                    document.querySelector('#backdrop_' + boxData.content_id).style.display='block';
+                }
+                document.querySelector('#box_' + boxData.content_id).style.display='block';
 
-                        document.querySelector("#box_" + boxData.content_id).addEventListener('animationend', () => {
-                            console.log('animasyon bitti');
-                            if (boxData.animations[animationData.animation_name].remove_after==="true" || boxData.animations[animationData.animation_name].remove_after===true) {
-                                this.removeBox(boxData)
-                                console.log('sadece animasyon bitti')
+                console.log('Eklenecek olan animasyonun adi:'+animationData.animation_name[this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id)] + "_" + boxData.content_id);
+                document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name[this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id)] + "_" + boxData.content_id);
+            });
+        });
+    } else {
+        document.querySelector('#box_' + boxData.content_id).style.display='block';
+        let indexOfAnimationInSwitch = (this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id)+1)%2;
+        document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name[indexOfAnimationInSwitch] + "_" + boxData.content_id);
+    }
+},
+    switchPositionConvertor(switchID){
+        return (document.querySelector(switchID) && document.querySelector(switchID).checked)?1:0;
+    },
+    scenarioRunner(boxData, scenarioName){
 
-                            }
-                                if(animIndex===scenario.animations.length-1){
-                                    //senaryonun son animasyonu ve bu animasyon repetetive degilse eventlistener iptal edilecek
-                                    triggerElement.removeEventListener(scenario.event, animApplicator, {passive:false});
-                                    //*******************************************************************************************
-                                }
+        let scenario = boxData.scenarios[scenarioName];
+        const triggerElement = (scenario.event_source!=="window")?document.querySelector(scenario.event_source +'_' + boxData.content_id):document;
+
+        scenario.animations.forEach(async (animationData, animIndex) => {
+            let indexOfAnimationInSwitch = this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id);
+
+            this.createAnimation(boxData, animationData); //CSS animasyonlari olusturulup style icine gomuldu
+
+            if(scenario.event_source==="window" && scenario.event==='load'){ // window.load ise dogrudan class ekle (CSS animasyon olusmustu zaten)
+                document.querySelector("#box_" + boxData.content_id).style.display='';
+                document.querySelector("#box_" + boxData.content_id).classList.add(animationData.animation_name + "_" + boxData.content_id);
+            }
+
+            //repetative degerine gore tek sefer implemente edilecek veya requestAnimationFrame kullanilacak
+            // triggerElement.addEventListener(scenario.event,animApplicator());
+            triggerElement.addEventListener(scenario.event,()=>{
+                this.animApplicator(boxData,scenarioName,animationData);
+            });
+
+            document.querySelector("#box_" + boxData.content_id).addEventListener('animationend', () => {
+                    if (boxData.animations[animationData.animation_name[0]].remove_after==="true" || boxData.animations[animationData.animation_name[this.switchPositionConvertor(boxData.scenarios[scenarioName].event_source+'_'+boxData.content_id)]].remove_after===true) {
+                        this.removeBox(boxData)
+
+                    }
+                    if(animIndex===scenario.animations.length-1){
+                        //senaryonun son animasyonu ve bu animasyon repetetive degilse eventlistener iptal edilecek
+                        let isRepetitive = scenario.animations[animIndex].repetitive
+                        if(isRepetitive==="false" || isRepetitive===false){
+                            triggerElement.removeEventListener(scenario.event, this.animApplicator, {passive:false});
                         }
-                        );
-                    })
-            //exceptions (for event bubbling)
-            scenario.except.forEach(itemIdPrefix=>{
-                console.log(itemIdPrefix +'_' + boxData.content_id);
-                document.querySelector(itemIdPrefix +'_' + boxData.content_id).addEventListener(scenario.event,(event)=>{
-                    event.stopPropagation();
-                })
+                        //*******************************************************************************************
+                    }
+                }
+            );
+        })
+        //exceptions (for event bubbling)
+        scenario.except.forEach(itemIdPrefix=>{
+            document.querySelector(itemIdPrefix +'_' + boxData.content_id).addEventListener(scenario.event,(event)=>{
+                event.stopPropagation();
             })
         })
+
+    },
+    action(boxData){
+        // tum boxdatasi senaryolarini loop ile isleyen fonksiyon bu, tek tek isleyen fonksiyona cagrida bulunacak
+        Object.keys(boxData.scenarios).forEach(scenarioName=> {
+            this.scenarioRunner(boxData, scenarioName);
+        });
     },
     removeBox(boxData) {
         if (boxData.backdrop === true || boxData.backdrop === "true") {
